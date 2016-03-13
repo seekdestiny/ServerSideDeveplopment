5459f1619a4df78faa523422789bbc2f00bf9bc6
seekdestiny <jifeiqian@gmail.com>
2016-03-12 16:48:55 -0800

    complete assign 1

---
+++ app/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.use('/dishes', require('./dishRouter'));
+app.use('/promotions', require('./promoRouter'));
+app.use('/leadership', require('./leaderRouter'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
