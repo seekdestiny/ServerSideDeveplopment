c31ac82daa92305635e6af946381a59722ba1de4
seekdestiny <jifeiqian@gmail.com>
2016-03-19 18:23:18 -0700

    use oauth for facebook login

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1179339438772826',
+        clientSecret: '68f0a5a91077712c1bff15691c4820fc',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
--- rest-server-passport/routes/users.js
+++ rest-server-passport/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
-var Verify    = require('./verify');
+var Verify = require('./verify');
 
 /* GET users listing. */
 router.route('/')
@@ -67,6 +67,35 @@ router.get('/logout', function(req, res) {
   res.status(200).json({
     status: 'Bye!'
   });
+});
+
+router.get('/facebook', passport.authenticate('facebook'),
+  function(req, res){});
+
+router.get('/facebook/callback', function(req,res,next){
+  passport.authenticate('facebook', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({
+        err: info
+      });
+    }
+    req.logIn(user, function(err) {
+        if (err) {
+            return res.status(500).json({
+                err: 'Could not log in user'
+            });
+        }
+        var token = Verify.getToken(user);
+        res.status(200).json({
+            status: 'Login successful!',
+            success: true,
+            token: token
+        });
+    });
+  })(req,res,next);
 });
 
 module.exports = router;
